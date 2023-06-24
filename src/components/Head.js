c6adc1e276af72eceea3d5ec5ddd38c5f91a1393
eggshell <edzeljohnsinfuego@gmail.com>
2023-06-24 12:51:06 +0800

    try seo

--- src/components/Head.js
+++ src/components/Head.js
@@ -7,7 +7,7 @@ class Head extends Helmet {
           <div>
             <Helmet>
               <title>PSU Trulab</title>
-              <meta name="description" content="Your page description" />
+              <meta name="description" content="Border is me" />
               <meta property="og:url" content="URL_OF_YOUR_PAGE" />
               <meta property="og:type" content="for fun" />
               <meta property="og:title" content="PARSU Trulab Finder" />
