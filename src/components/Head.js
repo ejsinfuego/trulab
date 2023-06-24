8e5ec83a3468eeb6965a19405e4063b7a0fcd16c
eggshell <edzeljohnsinfuego@gmail.com>
2023-06-24 12:54:52 +0800

    try meta keys

--- src/components/Head.js
+++ src/components/Head.js
@@ -7,13 +7,14 @@ class Head extends Helmet {
           <div>
             <Helmet>
               <title>PSU Trulab</title>
-              <meta name="description" content="Border is me" />
+             
+            </Helmet>
+              <meta name="description" content="Border is" />
               <meta property="og:url" content="URL_OF_YOUR_PAGE" />
               <meta property="og:type" content="for fun" />
               <meta property="og:title" content="PARSU Trulab Finder" />
               <meta property="og:description" content="Bored ako e haha" />
               <meta property="og:image" content="URL_OF_IMAGE_TO_BE_SHARED" />
-            </Helmet>
               
           </div>
         );
